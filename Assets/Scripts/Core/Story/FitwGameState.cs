441e1265a058c3d302bb6138aeb045a15f521c62
AaronSong321 <25111456542@qq.com>
2021-02-20 21:45:54 +0000

    init; add serialization

---
+++ Assets/Scripts/Core/Story/FitwGameState.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+$HEADER$namespace $NAMESPACE$
+{
+  public class $CLASS$ {$END$}
+}
